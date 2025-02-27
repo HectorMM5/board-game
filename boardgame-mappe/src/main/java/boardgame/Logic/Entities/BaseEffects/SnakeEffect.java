ba48fa129302b746baf0a4bd22f1985b223594f6
Hector <hectome@ntnu.no>
2025-02-27 16:49:24 +0100

    Modified into createMovementEffect, created setupEffects. Created SnakeEffect (empty)

---
+++ boardgame-mappe/src/main/java/boardgame/Logic/Entities/BaseEffects/SnakeEffect.java
@@ -0,0 +1,20 @@
+package boardgame.Logic.Entities.BaseEffects;
+
+import boardgame.Logic.Entities.Effect;
+import boardgame.Logic.Entities.Player;
+
+public class SnakeEffect implements Effect {
+
+    @Override
+    public void setup() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void execute(Player player) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
--- boardgame-mappe/src/main/java/boardgame/Logic/Functionality/Board.java
+++ boardgame-mappe/src/main/java/boardgame/Logic/Functionality/Board.java
@@ -7,10 +7,12 @@ import java.util.stream.IntStream;
 
 import boardgame.Logic.Entities.BaseEffects.LadderEffect;
 import boardgame.Logic.Entities.BaseEffects.PlaceholderEffect;
+import boardgame.Logic.Entities.BaseEffects.SnakeEffect;
 import boardgame.Logic.Entities.Effect;
 import boardgame.Logic.Entities.Player;
 import boardgame.Logic.Entities.Rules;
 import boardgame.Logic.Entities.Tile;
+import boardgame.Logic.Functionality.Board.effectType;
 
 
 public class Board {
@@ -43,23 +45,49 @@ public class Board {
 
     }
 
-    public void createLadderEffect() {
 
-        //Two tiles are randomly selected
-        Tile baseTile = tiles.get(randomGenerator.nextInt(1, 100));
-        Tile targetTile = tiles.get(randomGenerator.nextInt(baseTile.getNumber(), 100));
+    public enum effectType {
+        LADDER,
+        SNAKE
+    }
 
-        //Selected tiles may already have an effect; if so, reroll
-        while (baseTile.getEffect() != null || targetTile.getEffect() != null) {
-            baseTile = tiles.get(randomGenerator.nextInt(1, 100));
-            targetTile = tiles.get(randomGenerator.nextInt(baseTile.getNumber(), 100));
+    public void createMovementEffect(effectType type) {
+
+        Tile baseTile;
+        Tile targetTile;
+
+        if (null == type) {
+            throw new IllegalArgumentException("No valid enum given.");
+        } else switch (type) {
+            case LADDER:
+                //Two tiles are randomly selected
+                baseTile = tiles.get(randomGenerator.nextInt(1, 100));
+                //MUST BE FIXED WHEN BASETILE RNG GIVES HIGH NUMBER. MAY NEED MULTIIPLE REROLLS TO GET WITHIN THE LOW %
+                //eg. basetile = 97 has only 98, 99 and 100 as valid values, but rng runs from 1-100
+                targetTile = tiles.get(randomGenerator.nextInt(baseTile.getNumber(), 100));
+                //Selected tiles may already have an effect; if so, reroll
+                while (baseTile.getEffect() != null || targetTile.getEffect() != null) {
+                    baseTile = tiles.get(randomGenerator.nextInt(1, 101));
+                    targetTile = tiles.get(randomGenerator.nextInt(baseTile.getNumber(), 100));
+                }   break;
+            case SNAKE:
+                //Two tiles are randomly selected
+                baseTile = tiles.get(randomGenerator.nextInt(2, 101));
+                targetTile = tiles.get(randomGenerator.nextInt(1, baseTile.getNumber()));
+                //Selected tiles may already have an effect; if so, reroll
+                while (baseTile.getEffect() != null || targetTile.getEffect() != null) {
+                    baseTile = tiles.get(randomGenerator.nextInt(1, 101));
+                    targetTile = tiles.get(randomGenerator.nextInt(baseTile.getNumber(), 101));
+                }   break;
+            default:
+                throw new IllegalArgumentException("No valid enum given.");
         }
 
         LadderEffect effect = new LadderEffect(baseTile, targetTile);
-        baseTile.setEffect(effect);
-        targetTile.setEffect(new PlaceholderEffect());
-        effects.put(effects.size(), effect);
-
+            baseTile.setEffect(effect);
+            targetTile.setEffect(new PlaceholderEffect());
+            effects.put(effects.size(), effect);
+    
     }
 
     public void setupEffects() {
@@ -67,7 +95,14 @@ public class Board {
 
         //As long as total effect amount is less than 20% of tiles, this runs
         while (effects.size() < tiles.size() * 0.2) {
-            effectOptions.get(randomGenerator.nextInt(0, effectOptions.size()-1));
+            Effect chosenEffect = effectOptions.get(randomGenerator.nextInt(0, effectOptions.size()-1));
+
+            if (chosenEffect.getClass() == LadderEffect.class) {
+                createMovementEffect(effectType.LADDER);
+                
+            } else if (chosenEffect.getClass() == SnakeEffect.class) {
+                createMovementEffect(effectType.SNAKE);
+            }
 
         }
     }
