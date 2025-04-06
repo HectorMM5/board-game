414136b1b72d327bf63e3cbbf3adaf5b40014b0f
zomeiro <ba.vangen5@gmail.com>
2025-04-06 20:23:30 +0200

    Added class to save BoardCollection object to JSON

---
+++ boardgame-mappe/src/main/java/boardgame/controller/BoardJSON.java
@@ -0,0 +1,29 @@
+package boardgame.controller;
+
+import java.io.File;
+import java.io.IOException;
+import boardgame.model.boardFiles.Board;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class BoardJSON {
+
+    private final ObjectMapper objectMapper;
+    private final String filePath;
+
+    public BoardJSON(String filePath) {
+        this.objectMapper = new ObjectMapper();
+        this.objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        this.filePath = filePath;
+    }
+
+    public void save(BoardCollection boardCollection) throws IOException {
+            objectMapper.writeValue(new File(filePath), boardCollection);
+    }
+
+    public BoardCollection read() throws IOException {
+
+            return objectMapper.readValue(new File(filePath), BoardCollection.class);
+    }
+}
