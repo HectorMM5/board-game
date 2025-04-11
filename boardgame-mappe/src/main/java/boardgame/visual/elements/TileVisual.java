83091f9ba9c6a2a6b6e50777cde8666afa2717a4
Hector <hectome@ntnu.no>
2025-04-11 15:21:38 +0200

    JavaDocs

--- boardgame-mappe/src/main/java/boardgame/visual/elements/TileVisual.java
+++ boardgame-mappe/src/main/java/boardgame/visual/elements/TileVisual.java
@@ -14,13 +14,44 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * <p>
+ * A visual representation of a {@code Tile} in the board game. This component
+ * is a {@code StackPane} that layers classic visual elements, such as:
+ * </p>
+ * <ul>
+ * <li>A background color</li>
+ * <li>The tile number</li>
+ * </ul>
+ *
+ * <p>
+ * In cases where there are players in the tile, the following elements also
+ * apply:
+ * </p>
+ * <ul>
+ * <li>A player icon (if one player is present)</li>
+ * <li>An overlay and player counter (if multiple players are present)</li>
+ * </ul>
+ *
+ * <p>
+ * The visual is updated using {@link #updateVisual()} based on the tile state.
+ * </p>
+ *
+ * @author Hector Mendana Morales
+ */
 public class TileVisual extends StackPane {
+
     private final Tile tile;
     private final Rectangle background;
     private final ImageView iconView;
     private final Pane cover;
     private final Label playersInTile;
 
+    /**
+     * Constructs a new {@code TileVisual} based on paramet.
+     *
+     * @param tile the {@code Tile} object this visual represents
+     */
     public TileVisual(Tile tile) {
         this.tile = tile;
         this.background = new Rectangle(50, 50);
@@ -29,7 +60,7 @@ public class TileVisual extends StackPane {
         this.iconView = new ImageView();
         iconView.setFitWidth(40);
         iconView.setFitHeight(40);
-        
+
         Label viewNumber = new Label(Integer.toString(tile.getNumber()));
 
         this.cover = new Pane();
@@ -38,16 +69,29 @@ public class TileVisual extends StackPane {
 
         this.playersInTile = new Label();
 
-
         this.getChildren().addAll(background, viewNumber, iconView, cover, playersInTile);
 
     }
 
+    /**
+     * <p>
+     * Updates the visual elements of this tile based on its current state:
+     * <p>
+     *
+     * <ul>
+     * <li>If the tile has an effect, the background becomes red.</li>
+     * <li>If one player is on the tile, their icon is shown.</li>
+     * <li>If multiple players are on the tile, a white overlay and a count are
+     * shown.</li>
+     *
+     * <li>If no players are present, the tile number is shown.</li>
+     * </ul>
+     */
     public void updateVisual() {
         if (tile.getEffect() != null) {
             this.background.setFill(Color.rgb(205, 84, 84));
         }
-        
+
         List<Player> players = tile.getPlayers();
 
         if (!players.isEmpty()) {
@@ -59,26 +103,28 @@ public class TileVisual extends StackPane {
                 iconView.setImage(playerIcon);
                 playersInTile.setText("");
                 cover.setBackground(Background.EMPTY);
-            }
-
-            else {
+            } else {
                 cover.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
                 playersInTile.setText(Integer.toString(players.size()));
 
             }
-            
 
         } else if (iconView.getImage() != null || playersInTile.getText().equals("")) {
             iconView.setImage(null);
             playersInTile.setText("");
             cover.setBackground(Background.EMPTY);
-            
+
         }
 
     }
 
+    /**
+     * Returns the {@code Tile} model associated with this visual.
+     *
+     * @return the {@code Tile} this visual represents
+     */
     public Tile getTile() {
         return tile;
     }
 
-}  
+}
