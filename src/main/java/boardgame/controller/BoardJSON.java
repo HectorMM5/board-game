81307c0db1a532e2f62aa92ec77fda0b75c7690f
Hector <hektor.mendana@gmail.com>
2025-04-22 21:54:00 +0200

    working creation of board from JSON

--- src/main/java/boardgame/controller/BoardJSON.java
+++ src/main/java/boardgame/controller/BoardJSON.java
@@ -1,29 +1,81 @@
 package boardgame.controller;
 
-import boardgame.model.boardFiles.BoardCollection;
-import java.io.File;
 import java.io.IOException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.IntStream;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import boardgame.model.boardFiles.Board;
+import boardgame.model.effectFiles.BackToStartEffect;
+import boardgame.model.effectFiles.Effect;
+import boardgame.model.effectFiles.LadderEffect;
+import boardgame.model.effectFiles.PlaceholderEffect;
+import boardgame.model.effectFiles.SkipTurnEffect;
+import boardgame.model.effectFiles.SnakeEffect;
 
 public class BoardJSON {
 
-    private final ObjectMapper objectMapper;
-    private final String filePath;
+    GameController gameController;
+    public static Board constructSnLBoardFromJSON(int choice, GameController gameController) {
+        Board board = new Board();
 
-    public BoardJSON(String filePath) {
-        this.objectMapper = new ObjectMapper();
-        this.objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        try {
+            String jsonText = Files.readString(Paths.get("boards.json"));
+            JSONObject SnLBoard = new JSONObject(jsonText).getJSONArray("games").getJSONObject(0).getJSONArray("SnL").getJSONObject(choice);
 
-        this.filePath = filePath;
+            JSONArray tilesWithEffects = SnLBoard.getJSONArray("tiles"); 
+
+            IntStream.rangeClosed(0, tilesWithEffects.length() - 1)
+            .forEach(i -> modifyEffectTileFromJSON(tilesWithEffects.getJSONObject(i), board, gameController));
+
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return board;
     }
 
-    public void save(BoardCollection boardCollection) throws IOException {
-            objectMapper.writeValue(new File(filePath), boardCollection);
+    public static void modifyEffectTileFromJSON(JSONObject tileWithEffect, Board board, GameController gameController) {
+        int tileNumber = tileWithEffect.getInt("tile");
+        String effectType = tileWithEffect.getString("effect");
+        
+        Effect effect;
+        int target;
+        switch (effectType) {
+            case "Ladder":
+                target = tileWithEffect.getInt("target");
+                board.getTiles().get(tileNumber - 1).setEffect(new PlaceholderEffect());
+
+                effect = new LadderEffect(gameController, tileNumber, target);
+
+                break;
+
+            case "Snake":
+                target = tileWithEffect.getInt("target");
+                board.getTiles().get(tileNumber - 1).setEffect(new PlaceholderEffect());
+
+                effect = new SnakeEffect(gameController, tileNumber, target);
+
+                break;
+
+            case "LoseTurn":
+                effect = new SkipTurnEffect(gameController);
+                break;
+
+            case "Back":
+                effect = new BackToStartEffect(gameController);
+                break;
+
+            default:
+                throw new AssertionError();
+        }
+
+        board.getTiles().get(tileNumber - 1).setEffect(effect);
+
     }
 
-    public BoardCollection read() throws IOException {
-
-            return objectMapper.readValue(new File(filePath), BoardCollection.class);
-    }
 }
