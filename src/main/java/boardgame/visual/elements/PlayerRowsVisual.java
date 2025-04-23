4c04b5b829a48be53a096d0fa7b141ca5d4229c9
HectorMM5 <hektor.mendana@gmail.com>
2025-04-23 16:09:30 +0200

    Now creates the entire player table instead of singular rows

--- src/main/java/boardgame/visual/elements/PlayerRowsVisual.java
+++ src/main/java/boardgame/visual/elements/PlayerRowsVisual.java
@@ -1,48 +1,43 @@
 package boardgame.visual.elements;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.model.boardFiles.Player;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 
-public class PlayerRowVisual {
+public class PlayerRowsVisual extends VBox {
+    private List<HBox> playerRows = new ArrayList<>();
 
-    private HBox row;
-    private Circle turnCircle;
+    public PlayerRowsVisual(List<Player> players) {
+        players.stream().forEach(i -> createPlayerRow(i));
+    }
 
-    public PlayerRowVisual(Player player) {
-        row = new HBox();
-
+    public void createPlayerRow(Player player) {
+        HBox row = new HBox();
         row.setBackground(Background.fill(Color.WHITE));
 
         ImageView playerIcon = new ImageView(new Image(getClass().getResourceAsStream(player.getIcon())));
 
         playerIcon.setFitWidth(50);
         playerIcon.setFitHeight(50);
-
         Label name = new Label(player.getName());
-
-        turnCircle = new Circle(3, Color.WHITE);
 
         row.getChildren().addAll(playerIcon, name);
 
+        this.getChildren().addAll(row);
 
     }
 
-    public void toggleTurn() {
-        turnCircle.setFill(Color.YELLOW);
-        
-    }
-
-    public HBox getRow() {
-        return row;
-    }
-
-    
+    public PlayerRowsVisual getPlayerRows() {
+        return this;
+    }  
  
     
 }
